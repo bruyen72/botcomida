f30997c314fb472a24e1814fa8e9d8c91c08050e
bruyen72 <brunoruthes92@gmail.com>
2025-11-27 14:11:04 -0400

    Fix QR connection: Update WhatsApp Web to v2.3000 (Jan 2025)

--- src/server.ts
+++ src/server.ts
@@ -91,7 +91,7 @@ function initializeBot() {
     },
     webVersionCache: {
       type: 'remote',
-      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
+      remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.3000.1014590669-alpha.html'
     }
   });
 
